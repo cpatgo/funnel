93024a9501b1195489477631adebff98670d7387
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-05 05:47:21 +0800

    https://basecamp.com/2357068/projects/9542773/todos/265776682 Fix

--- aem/manage/js/form.js
+++ aem/manage/js/form.js
@@ -3,7 +3,7 @@
 {include file="form.view.js"}
 {include file="form.delete.js"}
 {include file="form.search.js"}
-{include file="clipboard.min.js"}
+// {include file="clipboard.min.js"}
 
 var form_listfilter = {jsvar var=$listfilter};
 
