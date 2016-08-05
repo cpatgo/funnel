3d04a24a3c24a5d49bef7c7ce7f38ae2d252816a
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-06 01:48:48 +0800

    Copy to Clipboard

--- aem/manage/js/aem.js
+++ aem/manage/js/aem.js
@@ -1,16 +1,7 @@
-$(function() {
+jQuery( document ).ready(function( $ ) {
     var clipboard = new Clipboard('.btnCopy');
 
     clipboard.on('success', function(e) {
-        console.info('Action:', e.action);
-        console.info('Text:', e.text);
-        console.info('Trigger:', e.trigger);
-
-        e.clearSelection();
-    });
-
-    clipboard.on('error', function(e) {
-        console.error('Action:', e.action);
-        console.error('Trigger:', e.trigger);
+        alert('Form copied to clipboard.');
     });
 });
