240ae1dda5704d8f659edf57b4105c35faaeaa51
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-06 01:16:53 +0800

    https://basecamp.com/2357068/projects/9542773/todos/265778881 Add debug

--- aem/manage/js/aem.js
+++ aem/manage/js/aem.js
@@ -1,3 +1,16 @@
 $(function() {
-    new Clipboard('.btnCopy');
+    var clipboard = new Clipboard('.btnCopy');
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 });
