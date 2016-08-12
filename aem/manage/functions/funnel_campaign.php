88ac765e4ff1aa4e6d09757aaa0aae95df5276a7
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-13 05:33:21 +0800

    Rename landing page

--- aem/manage/functions/funnel_campaign.php
+++ aem/manage/functions/funnel_campaign.php
@@ -9,7 +9,7 @@ if($action === 'list_insert_post') list_insert_post();
 function list_insert_post() {
 
 	$user_id = $_SESSION['awebdesk_aweb_admin']['id'];
-	$query = sprintf("INSERT INTO awebdesk_landingpage (title, type, description, user_id, list_id, page_link, date_created) VALUES ( '%s', '%s', '%s', '%d', '%d', '%s', '%s')", 
+	$query = sprintf("INSERT INTO awebdesk_funnel_campaign (title, type, description, user_id, list_id, page_link, date_created) VALUES ( '%s', '%s', '%s', '%d', '%d', '%s', '%s')", 
 		$_POST['title'], $_POST['type'], $_POST['description'], $user_id, $_POST['list_id'], $_POST['page_link'], date('Y-m-d H:i:s'));
 
 	mysqli_query($GLOBALS["aem_con"], $query);
