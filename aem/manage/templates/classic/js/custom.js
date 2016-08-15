98e73c3fe784fe9092b3b2a69dbff43dcd262475
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-16 03:08:24 +0800

    Add refresh list button

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -38,26 +38,39 @@ jQuery(document).ready(function(){
 	    }
 	});
 
+	//COLLECTION OF FUNCTIONS
+	var aem_functions = {
+		get_lists 	: 	function() {
+			jQuery.ajax({
+		        method: "post",
+		        url: "../manage/templates/classic/ajax/custom.php",
+		        data: {
+		            'action':'get_lists'
+		        },
+		        dataType: 'json',
+		        success:function(result) {
+		            console.log(result);
+		            jQuery('body').find('#list_block').append(result);
+		            var select_list = jQuery("body").find("#landing-page-list-id");
+		            
+					select_list.html('');
+					jQuery.each(result.data, function(key, value) {
+					    select_list.append(jQuery("<option></option>").attr("value", value.id).text(value.name));
+					});
+		        },
+		        error: function(errorThrown){
+		            console.log(errorThrown);
+		        }
+		    });
+		}
+	};
+
 	//GET LISTS
-	jQuery.ajax({
-        method: "post",
-        url: "../manage/templates/classic/ajax/custom.php",
-        data: {
-            'action':'get_lists'
-        },
-        dataType: 'json',
-        success:function(result) {
-            console.log(result);
-            jQuery('body').find('#list_block').append(result);
-            var select_list = jQuery("body").find("#landing-page-list-id");
-            
-			select_list.html('');
-			jQuery.each(result.data, function(key, value) {
-			    select_list.append(jQuery("<option></option>").attr("value", value.id).text(value.name));
-			});
-        },
-        error: function(errorThrown){
-            console.log(errorThrown);
-        }
+	aem_functions.get_lists();
+
+    //REFRESH LIST
+    jQuery('body').on('click', '#refresh_list', function(e){
+    	e.preventDefaults();
+    	aem_functions.get_lists();
     });
 });
