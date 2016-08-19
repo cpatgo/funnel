cc2598b122a676b5a1271c9692374ba9160050de
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-08-20 03:51:06 +0800

    Fix page

--- aem/manage/assets/home.php
+++ aem/manage/assets/home.php
@@ -7,7 +7,7 @@ require_once adesk_admin("functions/template.php");
 class home_assets extends AWEBP_Page {
 
 	function home_assets() {
-		$this->pageTitle = _a("Choose Campaign Type");
+		$this->pageTitle = _a("Getting Started With the GLC E-Markter");
 		$this->AWEBP_Page();
 	}
 
@@ -16,11 +16,6 @@ class home_assets extends AWEBP_Page {
 
 		if (!$this->admin["pg_message_add"] && !$this->admin["pg_message_edit"]) {
 			$smarty->assign('content_template', 'noaccess.htm');
-			return;
-		}
-
-		if ( list_get_cnt() == 0 ) {
-			$smarty->assign('content_template', 'nolists.htm');
 			return;
 		}
 
