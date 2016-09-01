fc78bd1f50dc2cd34241994755ba9f1f4c8c39c3
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-09-02 02:57:18 +0800

    Add custom redirect link

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -379,6 +379,14 @@ jQuery(document).ready(function(){
     	aem_functions.add_new_form();
     });
 
+    $body.on('keyup', '#sub2_redirect_input', function(e){
+    	jQuery(this).closest('input[name=sub2_redirect]').val(jQuery(this).val());
+    });
+
+    $body.on('keyup', '#sub3_redirect_input', function(e){
+    	jQuery(this).closest('input[name=sub3_redirect]').val(jQuery(this).val());
+    });
+
 	//GET FORMS
 	// aem_functions.get_forms();
 
