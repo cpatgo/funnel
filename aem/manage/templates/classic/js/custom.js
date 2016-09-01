e06bea892ab556aa3038454ff568baa8e6b04873
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-09-02 04:11:00 +0800

    Fix jquery

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -381,13 +381,13 @@ jQuery(document).ready(function(){
 
     $body.on('input', '#sub2_redirect_input', function(e){
     	e.preventDefault();
-    	jQuery('input[name=sub2_redirect]').attr('checked', true);
+		jQuery(this).closest('label').find('input[name=sub2_redirect]').attr('checked', true);
     	jQuery(this).closest('label').find('input[name=sub2_redirect]').val(jQuery(this).val());
     });
 
     $body.on('input', '#sub3_redirect_input', function(e){
     	e.preventDefault();
-    	jQuery('input[name=sub3_redirect]').attr('checked', true);
+    	jQuery(this).closest('label').find('input[name=sub3_redirect]').attr('checked', true);
     	jQuery(this).closest('label').find('input[name=sub3_redirect]').val(jQuery(this).val());
     });
 
