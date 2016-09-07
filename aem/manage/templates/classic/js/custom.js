ec42b770a010e30e8136280b5ed7c54482b64a9c
ghostman <cielbleuph@gmail.com>
2016-09-08 01:18:01 +0800

    fix radio button.

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function(){
 //      // stepsOrientation: "vertical"
 //  });
 //  
-    jQuery('#my-checkbox').on('switchChange.bootstrapSwitch', function (e, state) {
+    jQuery('[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function (e, state) {
 
             if(state){
                         alert("Checked");
