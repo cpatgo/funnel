e755c5de5a17c626d4ba8f09665cb514fbb37ebc
ghostman <cielbleuph@gmail.com>
2016-09-21 00:04:46 +0800

    updated jquery on step 5.

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -229,9 +229,10 @@ jQuery(document).ready(function(){
                 jQuery('.actions > ul > li:first-child').attr('style', 'display:none');
             }
 
+            console.log(current);
             if (current == 4) {
-                jQuery('.actions > ul > li:last-child').attr('disabled', 'disabled');
-                jQuery('.actions > ul > li:last-child').hide();
+                jQuery('.actions > ul > li:nth-child(2)').attr('disabled', 'disabled');
+                jQuery('.actions > ul > li:nth-child(2)').hide();
             }
         },
         onFinishing: function (event, currentIndex)
