de03fe2bdd3b4a9a4bea2c0a3f2484df5793fc72
ghostman <cielbleuph@gmail.com>
2016-09-24 02:36:42 +0800

    update progressbar colors.

--- aem/manage/templates/classic/js/custom.js
+++ aem/manage/templates/classic/js/custom.js
@@ -206,7 +206,7 @@ jQuery(document).ready(function(){
             ctr = current * 8.333333333334;
 
             // Math.round(price / listprice * 100) / 100
-            jQuery('#step-progressbar').progressbar({value: ctr});
+            jQuery('#step-progressbar').progressbar({value: ctr*next});
             
             // update the progressbar percentage text
             // $body.find('.progress_indicator_txt span').html( (+ctr).toFixed(2) + '%');
