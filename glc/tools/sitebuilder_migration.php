61d889ed4a97525f93934c02bba9b3a876ed8c72
ghostman <cielbleuph@gmail.com>
2016-09-27 21:35:10 +0800

    add sitebuilder_migration.php

---
+++ glc/tools/sitebuilder_migration.php
@@ -0,0 +1,14 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/wp-config.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/glc/config.php');
+
+// if(!isset($_GET['do'])) printf('<script type="text/javascript">window.location="%s/glc/admin";</script>', GLC_URL);
+
+$user_class = getInstance('Class_User');
+$users = get_users("orderby=ID");
+
+foreach($users as $key => $value) {
+	printf('%s - %s', $key, $value);
+}
+
+?>
