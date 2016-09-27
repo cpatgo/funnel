d2509234e073ac9e3c889eb9ec50491e24ecae8f
ghostman <cielbleuph@gmail.com>
2016-09-27 22:44:32 +0800

    updated

--- glc/tools/sitebuilder_migration.php
+++ glc/tools/sitebuilder_migration.php
@@ -4,20 +4,18 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/glc/config.php');
 
 // if(!isset($_GET['do'])) printf('<script type="text/javascript">window.location="%s/glc/admin";</script>', GLC_URL);
 
-
-
-
 $user_class = getInstance('Class_User');
 $users = get_users("orderby=ID");
 
-
-
-print_r($users);
-
-die('fin');
+// print_r($users);
+// die('fin');
 
 foreach($users as $key => $value) {
-	printf('%s - %s', $key, $value);
+	// printf('%s - %s', $key, $value);
+	$glc_user = $user_class->get_by_username($value->data->user_login);
+	$glc_user = $glc_user[0];
+	printf('%s', $glc_user);	
+	
 }
 
 ?>
