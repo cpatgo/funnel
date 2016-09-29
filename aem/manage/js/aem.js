a389ca2aef6a36233c0618c56dca13747333a344
ghostman <cielbleuph@gmail.com>
2016-09-30 06:06:12 +0800

    update to select multiple instances for menu.

--- aem/manage/js/aem.js
+++ aem/manage/js/aem.js
@@ -17,7 +17,7 @@ jQuery( document ).ready(function( $ ) {
     });
 
     if(typeof $_GET["action"] != "undefined") {
-    	$('body').find('li[id="menu_'+$_GET["action"]+'"]').addClass("active");
+    	$('body').find('li[id="menu_' + $_GET["action"] + '"]').addClass("active");
     } else {
     	$('body').find('li[id="menu_dashboard"]').addClass("active");	
     }
