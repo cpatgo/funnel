a81a0697ef54174f4d440f57bf2a88c4214631af
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-10-18 01:58:10 +0800

    Upload builder template files

---
+++ aem/manage/assets/builder_templates.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once adesk_admin("functions/database.php");
+require_once awebdesk_classes("select.php");
+require_once awebdesk_classes("pagination.php");
+require_once awebdesk_functions("file.php");
+
+class builder_templates extends AWEBP_Page {
+	function database_assets() {
+		$this->pageTitle = _a("Builder Templates");
+		$this->AWEBP_Page();
+		$this->admin = $GLOBALS["admin"];
+	}
+
+	function process(&$smarty) {
+		$this->setTemplateData($smarty);
+		if (isset($GLOBALS["_hosted_account"])) {
+			adesk_smarty_noaccess($smarty, $this);
+			return;
+		}
+
+		$smarty->assign("content_template", "builder_templates.htm");
+
+	}
+}
+
+?>
