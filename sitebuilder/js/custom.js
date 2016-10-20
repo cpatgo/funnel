18405a3d9b520d52065a46db3dc09047c052f1e6
ghostman <cielbleuph@gmail.com>
2016-10-21 03:35:02 +0800

    updated elements.json

--- sitebuilder/js/custom.js
+++ sitebuilder/js/custom.js
@@ -16,7 +16,7 @@
 
 	// $('body').find('#thank_you').parent().html('</li></ul><h3><span class="fui-list menu_categ"> DIY</span></h3><ul id="elementCats">' + '<li><a href id="navigation">Navigation</a></li></ul>');
 	// $('body').find("#elementCats").append('<h3 style="margin-top:30px;"><span class="fui-document"></span> Templates</h3><ul id="elementsCats" styles="margin-bottom: 30px;"><li><a href="#" id="travel">Travel</a></li></ul><hr>');
-	$('body').find('#elementCats').after('<h3 style="margin-top:30px;"><span class="fui-document"></span> Templates</h3><ul id="elementsCats" styles="margin-bottom: 30px;"><li><a href="#" id="travel">Travel</a></li></ul>');
+	$('body').find('#elementCats').after('<h3 style="margin-top:30px;"><span class="fui-document"></span> Templates</h3><ul id="elementCats" styles="margin-bottom: 30px;"><li><a href="#" id="travel">Travel</a></li></ul>');
 	
     
 }());
