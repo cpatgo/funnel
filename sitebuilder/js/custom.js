ac17a12ebd802f432f0722d05814f80b4b793772
ghostman <cielbleuph@gmail.com>
2016-10-21 03:01:21 +0800

    updated elements.json

--- sitebuilder/js/custom.js
+++ sitebuilder/js/custom.js
@@ -15,7 +15,7 @@
 	console.log('testing');
 
 	// $('body').find('#thank_you').parent().html('</li></ul><h3><span class="fui-list menu_categ"> DIY</span></h3><ul id="elementCats">' + '<li><a href id="navigation">Navigation</a></li></ul>');
-	$('body').find("#elementCats").append('<h3 style="margin-top:30px;"><span class="fui-document"></span> Blocks</h3><ul id="elementsCats" styles="margin-bottom: 30px;"><li><a href id="navigation">Navigation</a></li></ul>');
+	$('body').find("#elementCats").append('<hr><h3 style="margin-top:30px;"><span class="fui-document"></span> Templates</h3><ul id="elementsCats" styles="margin-bottom: 30px;"><li><a href id="navigation">Navigation</a></li></ul><hr>');
 	
     
 }());
