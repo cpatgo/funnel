3315fa6e467b737827054f26a7d1d1cd9608932d
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-10-22 06:08:57 +0800

    Upload finished templates

--- sitebuilder/elements/js/glc-event3/main.js
+++ sitebuilder/elements/js/glc-event3/main.js
@@ -1,13 +1,13 @@
  $(function() {
-        var endDate = "July 25, 2025 15:03:25";
+    var endDate = $('#endDate').val();
 
-       
-        $('.countdown.styled').countdown({
-          date: endDate,
-          render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.years, 4) + " <span>years</span></div><div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-          }
-        });
-        
+   
+    $('.countdown.styled').countdown({
+      date: endDate,
+      render: function(data) {
+        $(this.el).html("<div>" + this.leadingZeros(data.years, 4) + " <span>years</span></div><div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+      }
+    });
+    
 
-      });
+});
