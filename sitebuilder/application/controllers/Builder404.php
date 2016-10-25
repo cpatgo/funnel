0770e0f2caf41cba54737e15c5acd0a90f63c445
Sarah Gregorio <sarahgregorio29@gmail.com>
2016-10-26 05:18:45 +0800

    Add back button in 404 page

--- sitebuilder/application/controllers/Builder404.php
+++ sitebuilder/application/controllers/Builder404.php
@@ -8,8 +8,9 @@ class Builder404 extends CI_Controller
 
     public function index() 
     { 
+    	$data['return_link'] = $_SERVER['HTTP_REFERER'];
         $this->output->set_status_header('404'); 
-        $this->load->view('sites/builder404');
+        $this->load->view('sites/builder404', $data);
     } 
 } 
 ?> 
